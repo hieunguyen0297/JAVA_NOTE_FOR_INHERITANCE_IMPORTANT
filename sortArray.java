a693c8b8fd4e56e153088ee19c11f3d34c20479e
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-09-10 11:30:27 -0400

    Update sortArray.java

--- sortArray.java
+++ sortArray.java
@@ -3,6 +3,7 @@ public class MyClass {
     public static void main(String args[]) {
         int[] intArray = {10, 9, 8, 6, 8 ,11};
       
+        //the fastest is O(nLogn)
         //sort the above array with O(n2) time
         for (int i= 0; i < intArray.length - 1; i++){
             for(int y = i+ 1; y < intArray.length; y++){
