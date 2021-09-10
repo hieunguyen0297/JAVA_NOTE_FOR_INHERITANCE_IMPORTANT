0ad732bb85c6b405989e062107760205ec62daf3
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-09-10 11:29:24 -0400

    Add files via upload

---
+++ sortArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class MyClass {
+    public static void main(String args[]) {
+        int[] intArray = {10, 9, 8, 6, 8 ,11};
+      
+        //sort the above array with O(n2) time
+        for (int i= 0; i < intArray.length - 1; i++){
+            for(int y = i+ 1; y < intArray.length; y++){
+                int temp = intArray[i];
+                if(intArray[i] > intArray[y] ){
+                    intArray[i] = intArray[y];
+                    intArray[y] = temp;
+                }
+                
+                
+            }
+        }
+        for(int u : intArray){
+            System.out.print(u + " ");
+        }
+   
+     
+    }
+}
